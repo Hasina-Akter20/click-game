005468d6a4243bf35ba469c735edd90bb82fdb98
Hasina Akter <66660517+Hasina-Akter20@users.noreply.github.com>
2023-01-19 00:48:55 +0600

    project done

---
+++ script.js
@@ -0,0 +1,39 @@
+let showText = document.querySelector("h1");
+let counter = 0;
+let gameStarted = false;
+
+function clearDisply() {
+  let button = document.querySelector(".button-group");
+  button.style.display = "none";
+}
+
+function reloadAll() {
+  location.reload();
+}
+
+function reset() {
+  if (counter < 10) {
+    showText.innerText = "Ops, You Lose The Game";
+    setTimeout(reloadAll, 3000);
+  }
+}
+
+console.log(counter);
+
+function playGame() {
+  gameStarted = true;
+  setTimeout(reset, 2000);
+}
+
+function clickedCount() {
+  if (gameStarted) {
+    counter++;
+  }
+
+  showText.innerText = counter;
+  if (counter == 10) {
+    showText.innerText = "Congrass! You won the game ";
+    clearDisply();
+    setTimeout(reloadAll, 4000);
+  }
+}
